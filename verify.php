351dd711fe2bd0482282dd6356ccdc1eef8fe4a3
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-18 16:09:45 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
