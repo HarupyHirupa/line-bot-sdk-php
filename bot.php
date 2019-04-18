a7fd31cac55fe9d04e26e7219265106d0fd6a70d
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-18 21:54:49 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -13,8 +13,8 @@ if (!is_null($events['events'])) {
   // Reply only when message sent is in 'text' format
   if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
    // Get text sent
-   //$text = $event['message']['text'];
-	$text = 'Yepppppppp';
+   $text = $event['message']['text'];
+	//$text = 'Yepppppppp';
    // Get replyToken
    $replyToken = $event['replyToken'];
 
