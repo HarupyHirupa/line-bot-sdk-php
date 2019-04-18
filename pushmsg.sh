a230fbc069fc3fd915670c033f6d723016d67a0d
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-19 01:00:15 +0700

    Add files via upload

---
+++ pushmsg.sh
@@ -0,0 +1,18 @@
+
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=' \
+-d '{
+    "to": "C042ba72bd2b8ccdfccf9426a107cdfca",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, world1"
+        },
+        {
+            "type":"text",
+            "text":"Hello, world2"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/push
+
---
+++ runpush.php
@@ -0,0 +1,7 @@
+<?php
+      $tempFolder = '/tmp';
+      $webRootFolder = '/var/www';
+      $scriptName = 'pushmsg.sh';
+      $moveCommand = "mv $webRootFolder/$scriptName $tempFolder/$scriptName";
+      $output = shell_exec($moveCommand);
+?>
