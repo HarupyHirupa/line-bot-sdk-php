71e802892f5d2b935cd98d0beb1d45a0cfe06c78
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-19 00:53:23 +0700

    Add files via upload

--- pushmsg2.php
+++ pushmsg2.php
@@ -1,3 +1,5 @@
+<?php
+
 curl -X POST \
 -H 'Content-Type:application/json' \
 -H 'Authorization: Bearer oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=' \
