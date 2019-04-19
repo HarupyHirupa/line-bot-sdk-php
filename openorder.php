8140422df5e64a2cdf1123772524a56e1af30ae8
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-19 20:42:00 +0700

    Add files via upload

--- openorder.php
+++ openorder.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 function writeFile($data){
   $logPath = __DIR__. "pairTrade.txt";
   $mode = (!file_exists($logPath)) ? 'w':'a';
@@ -7,7 +7,27 @@ function writeFile($data){
   fwrite($logfile, "\r\n". $data);
   fclose($logfile);
 }
-//https://aps-superbot.herokuapp.com/openorder.php?task=upd_new_order&symbol=XAUUSD&op_price=1.123&tp_price=3.256&sl_price=5.678
+*/
+
+$server = "WIN08R2X64\sqlexpress";
+$user = "sa"; 
+$pass = "pr@159753"; 
+$db_name="FLIGHT_INFO";  
+$connectionInfo = array( "UID"=>$user,
+                         "PWD"=>$pass,
+                         "Database"=>$db_name);
+
+$objConnect = sqlsrv_connect($server, $connectionInfo) or die("Error Connect to Database");
+
+if($objConnect)  
+{  
+	echo "Database Connected.";  
+}  
+else  
+{  
+	echo "Database Connect Failed.";  
+} 
+
 
    $task = $_REQUEST['task'];
    //echo "Task = $task \r\n";
@@ -23,12 +43,50 @@ function writeFile($data){
 
    $sl_price = $_REQUEST['sl_price'];
    //echo "sl_price = $sl_price \r\n";
+
+   $op_type = $_REQUEST['op_type'];
+   //echo "op_type = $op_type \r\n";
+
+   $ticket_id = $_REQUEST['ticket_id'];
+   //echo "ticket_id = $ticket_id \r\n";
+
+   $preSQL = "INSERT INTO [FLIGHT_INFO].[dbo].[TB_OP_ORDER]";	
+	//echo $preSQL;	
 	
    if ($task === "upd_new_order")
    {
-	$saveData = '#Symbol='.$symbol.'#op_price='.$op_price.'#tp_price='.$op_price.'#sl_price='.$sl_price.'#notify=no';
-	writeFile($saveData);
+	$st_time = GETDATE();
+	$tsql_i = $preSQL." ([SYMBOL], [OP_PRICE], [TP_PRICE], [SL_PRICE], [OP_TYPE], [TICKET], [TIME_STAMP]) VALUES (?,?,?,?,?,?,?);";
+	$params_i = array(&$symbol,&$op_price,&$tp_price,&$sl_price,&$op_type,&$ticket_id,&$st_time);
+	// Prepare and execute the statement. 
+	if ($updateReview  = sqlsrv_prepare($objConnect , $tsql_i, $params_i))
+   	{
+		//echo "Pass prepare to update\n\r";
+   	}
+   	else
+   	{
+		//echo "Fail prepare to update\n\r";
+		die( print_r( sqlsrv_errors(), true));
+   	}
+
+	// By default, all stream data is sent at the time of
+	//query execution. 
+
+	if( sqlsrv_execute($updateReview ))
+ 	{
+		echo "#update OK#";
+   	}
+   	else
+  	{
+		echo "#update FAIL#";
+		die( print_r( sqlsrv_errors(), true));
+  	} 
+
    }
 
-echo 'OK';	
+
+sqlsrv_close($objConnect);
+
+?>
+
 	
