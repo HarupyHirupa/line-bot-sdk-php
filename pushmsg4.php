261f351bbe0ad298fc14a6b16641c71d1ca9359e
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-19 20:56:17 +0700

    Add files via upload

--- pushmsg4.php
+++ pushmsg4.php
@@ -10,7 +10,7 @@
    $replyData = curl_exec($curlSession);
    curl_close($curlSession);	
    
-   echo "raw=>".$replyData;		
+   echo raw=>$replyData;		
    if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
    //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
    $accessToken = "oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
