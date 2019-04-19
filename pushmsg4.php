df23acc663c3019dc8665d652019847e77661465
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-19 20:00:04 +0700

    Add files via upload

--- pushmsg4.php
+++ pushmsg4.php
@@ -16,7 +16,7 @@
    $accessToken = "oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    echo $replyData;
 
-
+/*
    
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
@@ -42,7 +42,7 @@
      curl_close($curlSession);
      return $retData;
    }
-
+*/
 		
    exit;
 ?>
