4c44ccdf15c4710e6a610878948240bd093ddc24
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-24 02:38:24 +0700

    Add files via upload

--- pushmsg430_group.php
+++ pushmsg430_group.php
@@ -12,7 +12,7 @@
    $groupID = substr($retData, $pos+1, strlen($retData)-$pos+1);	 
 
    //echo 'raw=>'.$replyData;		
-   if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
+   if(stripos($replyData,"Open Order",0) < 0) {$replyData = "No new order.!!";}		
    //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
    $accessToken = "W9XPAiTihrq4YYec21gDIEpts/88RGZc18uiz81uCykGu4kwSazkEgBvs8e0RuA/nUi0K2mcINn5ubtzOCnLFBc2NlE9DRLn+JE+az+MHtr8rW11X2vbn7PbEntBCv3GFuaAk3/Ordvix/E9pwJT2wdB04t89/1O/w1cDnyilFU=";
    //$groupID = "C41a4796d0c1af51d998d88d32eae52ba";
