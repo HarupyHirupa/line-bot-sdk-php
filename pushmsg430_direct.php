a3c90b8dc7520c42481a02400c703bc460b23818
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-23 14:59:10 +0700

    Add files via upload

--- pushmsg430_direct.php
+++ pushmsg430_direct.php
@@ -39,13 +39,14 @@
 		$replyData = "Open Order\n SELL : $symbol => $op_price\nTP => $tp_price\nSL => $sl_price\r\n";
 	}
    }				
-   echo 'raw=>'.$replyData;
-exit;		
+   //echo 'raw=>'.$replyData;
+		
    if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
    //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
    $accessToken = "W9XPAiTihrq4YYec21gDIEpts/88RGZc18uiz81uCykGu4kwSazkEgBvs8e0RuA/nUi0K2mcINn5ubtzOCnLFBc2NlE9DRLn+JE+az+MHtr8rW11X2vbn7PbEntBCv3GFuaAk3/Ordvix/E9pwJT2wdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    $groupID = "C042ba72bd2b8ccdfccf9426a107cdfca";
    echo $replyData;
+exit;
 	
    //$content = file_get_contents('php://input');
    //$arrayJson = json_decode($content, true);
