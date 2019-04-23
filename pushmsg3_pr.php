62e6bfd91add71684aa6dbc228c33a524dd27ee4
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-24 03:21:24 +0700

    Add files via upload

--- pushmsg3_pr.php
+++ pushmsg3_pr.php
@@ -1,5 +1,6 @@
 <?php
 
+  /*
    $curlSession = curl_init();
    curl_setopt($curlSession, CURLOPT_URL, 'http://tangmee.com/feedmepro/get_new_order.php?task=get_new_order');
    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
@@ -11,6 +12,7 @@
 	
    if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
    //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
+   */
    $accessToken = "W9XPAiTihrq4YYec21gDIEpts/88RGZc18uiz81uCykGu4kwSazkEgBvs8e0RuA/nUi0K2mcINn5ubtzOCnLFBc2NlE9DRLn+JE+az+MHtr8rW11X2vbn7PbEntBCv3GFuaAk3/Ordvix/E9pwJT2wdB04t89/1O/w1cDnyilFU=";
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
@@ -21,15 +23,17 @@
    $message = $arrayJson['events'][0]['message']['text'];
    
    $id = $arrayJson['events'][0]['source']['groupId'];
-
+   //echo $id;
    if($message == "request"){
 	$arrayPostData['to'] = $id;
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $replyData ;
+        //$arrayPostData['messages'][0]['text'] = $replyData ;
+	$arrayPostData['messages'][0]['text'] = $id;
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "2";
         $arrayPostData['messages'][1]['stickerId'] = "34";
         pushMsg($arrayHeader,$arrayPostData);
+/*
       for ($i=0; $i <= 30; $i++) {
          $replyData = callUrlData();
          if(strpos($replyData, "Open Order") == false) {
@@ -44,7 +48,7 @@
          $arrayPostData['messages'][1]['packageId'] = "2";
          $arrayPostData['messages'][1]['stickerId'] = "34";
          pushMsg($arrayHeader,$arrayPostData);
-      }
+      }  */
    }
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
