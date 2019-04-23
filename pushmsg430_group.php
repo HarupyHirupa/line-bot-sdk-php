c7161f716bb77151ddf86e6a45c04b54c0b8d221
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-24 02:28:25 +0700

    Add files via upload

--- pushmsg430_group.php
+++ pushmsg430_group.php
@@ -1,32 +1,23 @@
 <?php
 
-   $bot_name = "430 Signal";
-for($i=1;$i<=3;$i++)
-{
-   
+   //$g_url = 'http://tangmee.com/feedmepro/get_new_order_430.php?task=get_new_order&g_id='.$groupID;
    $curlSession = curl_init();
-   curl_setopt($curlSession, CURLOPT_URL, 'http://tangmee.com/feedmepro/get_group_id.php?task=get_g_id&bot_name='.$bot_name.'&rec_id='.$i);
+   curl_setopt($curlSession, CURLOPT_URL, 'http://tangmee.com/feedmepro/get_new_order_430.php?task=get_new_order');
    curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
    curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 
-   $groupID = curl_exec($curlSession);
-   curl_close($curlSession);
-   
-   $g_url = 'http://tangmee.com/feedmepro/get_new_order_430.php?task=get_new_order&g_id='.$groupID;
-   $curlSession = curl_init();
-   curl_setopt($curlSession, CURLOPT_URL, $g_url);
-   curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-   curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-
-   $replyData = curl_exec($curlSession);
+   $retData = curl_exec($curlSession);
    curl_close($curlSession);	
-   
+   $pos = stripos($retData, '#', 0);
+   $replyData = substr($retData, 0, $pos);
+   $groupID = substr($retData, $pos+1, strlen($retData)-$pos+1);	 
+
    //echo 'raw=>'.$replyData;		
    //if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
    //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
    $accessToken = "W9XPAiTihrq4YYec21gDIEpts/88RGZc18uiz81uCykGu4kwSazkEgBvs8e0RuA/nUi0K2mcINn5ubtzOCnLFBc2NlE9DRLn+JE+az+MHtr8rW11X2vbn7PbEntBCv3GFuaAk3/Ordvix/E9pwJT2wdB04t89/1O/w1cDnyilFU=";
    //$groupID = "C042ba72bd2b8ccdfccf9426a107cdfca";
-   echo $replyData;
+   echo $replyData.'|Group ID =>'.$groupID;
 	
    //$content = file_get_contents('php://input');
    //$arrayJson = json_decode($content, true);
@@ -40,7 +31,7 @@ for($i=1;$i<=3;$i++)
    $arrayPostData['messages'][1]['packageId'] = "2";
    $arrayPostData['messages'][1]['stickerId'] = "34";
    pushMsg($arrayHeader,$arrayPostData);
-}
+
 
 
    function pushMsg($arrayHeader,$arrayPostData){
@@ -57,22 +48,6 @@ for($i=1;$i<=3;$i++)
       curl_close ($ch);
    }
 	
-   function callUrlData(){
-     $curlSession = curl_init();
-     curl_setopt($curlSession, CURLOPT_URL, 'http://tangmee.com/feedmepro/get_new_order_test.php?task=get_new_order');
-     curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-     curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-
-     $replyData = curl_exec($curlSession);
-     curl_close($curlSession);	
-   
-     //echo 'raw=>'.$replyData;		
-     if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
-     //$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
-     $accessToken = "oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-     echo $replyData;
-   }
-
 		
    exit;
 ?>
