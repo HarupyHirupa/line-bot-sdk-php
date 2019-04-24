46d29fc1b908d17426057d9eda5220070cf46917
HarupyHirupa <49728127+HarupyHirupa@users.noreply.github.com>
2019-04-24 14:43:49 +0700

    Add files via upload

--- pushmsg4.php
+++ pushmsg4.php
@@ -23,24 +23,26 @@
    	curl_close($curlSession);	
    	
    	//echo 'raw=>'.$replyData;		
-   	if(strpos($replyData, "Open Order") == false) {$replyData = "No new order.!!";}		
-   	//$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
+   	
+	//$replyData = "Reply Test\nSELL:GBPUSD => 1.29852\nTP => 128652\nSL => 1.29452";
    	$accessToken = "oPkXa0tKzfxfMCjx6gm5iirMYaHeXia/Fsy1R9Lt8lRybMocm/seOqBvbIaHYkqtprR4DgHJcmsI6XNoatxGLYidiWJQEO0acDULgyJSHB2EOHNRAFXHxOuC0tP7KwiibUSgyuz6kB+MKKZf17qjYgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
    	//$groupID = "C042ba72bd2b8ccdfccf9426a107cdfca";
 	$groupID = $g_id;
    	echo $replyData;
 	
-   
-   	$arrayHeader = array();
-   	$arrayHeader[] = "Content-Type: application/json";
-   	$arrayHeader[] = "Authorization: Bearer {$accessToken}";
-   	$arrayPostData['to'] = $groupID;
-   	$arrayPostData['messages'][0]['type'] = "text";
-   	$arrayPostData['messages'][0]['text'] = $replyData ;
-   	$arrayPostData['messages'][1]['type'] = "sticker";
-   	$arrayPostData['messages'][1]['packageId'] = "2";
-   	$arrayPostData['messages'][1]['stickerId'] = "34";
-   	pushMsg($arrayHeader,$arrayPostData);
+   	if(strpos($replyData, "Open Order") == true) //{$replyData = "No new order.!!";}		
+   	{
+   		$arrayHeader = array();
+   		$arrayHeader[] = "Content-Type: application/json";
+   		$arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   		$arrayPostData['to'] = $groupID;
+   		$arrayPostData['messages'][0]['type'] = "text";
+   		$arrayPostData['messages'][0]['text'] = $replyData ;
+   		$arrayPostData['messages'][1]['type'] = "sticker";
+   		$arrayPostData['messages'][1]['packageId'] = "2";
+   		$arrayPostData['messages'][1]['stickerId'] = "34";
+   		pushMsg($arrayHeader,$arrayPostData);
+	}
    }
 
 
